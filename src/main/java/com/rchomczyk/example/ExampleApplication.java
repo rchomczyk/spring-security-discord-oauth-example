0af4e46609f8048cad8d53209c4d5632e4353829
Rafał Chomczyk <rchomczyk@icloud.com>
2022-10-16 07:36:35 +0200

    Initial commit

---
+++ src/main/java/com/rchomczyk/example/ExampleApplication.java
@@ -0,0 +1,12 @@
+package com.rchomczyk.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleApplication {
+
+	public static void main(String[] arguments) {
+		SpringApplication.run(ExampleApplication.class, arguments);
+	}
+}
