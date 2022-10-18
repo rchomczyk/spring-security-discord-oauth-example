9c6a11ddcb48a52d42c7c28198f75a3ad02a007d
Rafał Chomczyk <rchomczyk@icloud.com>
2022-10-18 16:16:56 +0200

    Add and use groovy scripting support in tests.

---
+++ src/test/java/com/rchomczyk/example/ExampleApplicationTest.groovy
@@ -0,0 +1,25 @@
+package com.rchomczyk.example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+
+import static org.assertj.core.api.Assertions.assertThat
+
+@SpringBootTest
+class ExampleApplicationTest {
+
+	private static final int EXPECTED_BEAN_DEFINITIONS_COUNT = 254;
+
+	@Test
+	void 'validate if context loads'(ApplicationContext context) {
+		assertThat(context)
+			.isNotNull()
+	}
+
+	@Test
+	void 'validate if context declares correct amount of bean definitions'(ApplicationContext context) {
+		assertThat(context.beanDefinitionCount)
+			.isEqualTo(EXPECTED_BEAN_DEFINITIONS_COUNT)
+	}
+}
